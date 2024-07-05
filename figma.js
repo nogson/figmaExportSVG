a8df8161ba473430e71937d97b2f00bfc2891df5
佐藤　文彦 <fumihiko_sato@post.pioneer.co.jp>
2024-07-05 10:08:17 +0900

    修正

--- figma.js
+++ figma.js
@@ -17,7 +17,7 @@ const download = async (url, path) => {
   fs.writeFileSync(path, data);
 };
 
-console.log("%o", JSON.stringify(PAYLOAD));
+console.log(typeof PAYLOAD);
 
 const getComponents = async () => {
   try {
