4066d22575b3f72089342c300a02b8f9fd242d32
佐藤　文彦 <fumihiko_sato@post.pioneer.co.jp>
2024-07-05 15:26:46 +0900

    修正

--- figma.js
+++ figma.js
@@ -7,7 +7,6 @@ dotenv.config();
 
 const FIGMA_FILE_KEY = process.env.FIGMA_FILE_KEY;
 const TOKEN = process.env.FIGMA_TOKEN;
-const PAYLOAD = process.env.PAYLOAD;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -16,8 +15,6 @@ const download = async (url, path) => {
   const data = await res.text();
   fs.writeFileSync(path, data);
 };
-
-console.log(typeof PAYLOAD);
 
 const getComponents = async () => {
   try {
@@ -52,6 +49,15 @@ const getSvgImages = async (ids) => {
 };
 
 async function main() {
+  // 同期読み込みの場合
+  try {
+    const data = fs.readFileSync("payload.json", "utf8");
+    const payload = JSON.parse(data);
+    console.log(payload);
+  } catch (err) {
+    console.error(err);
+  }
+
   // ディレクトリを削除して初期化
   await fs.promises.rm(`${__dirname}/assets`, { recursive: true, force: true });
   // ディレクトリを作成
