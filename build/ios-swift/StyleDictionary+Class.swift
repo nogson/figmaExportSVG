888890ce0e43d0820dcbaa38ae4cad717e39b6b3
nogson <nogson@users.noreply.github.com>
2024-07-09 08:03:11 +0000

    feat: update variables

---
+++ build/ios-swift/StyleDictionary+Class.swift
@@ -0,0 +1,14 @@
+
+//
+// StyleDictionary+Class.swift
+//
+
+// Do not edit directly, this file was auto-generated.
+
+
+import UIKit
+
+public class StyleDictionaryClass {
+    public static let colorColorDark = rgba(0,0,0,1)
+    public static let colorColorLight = rgba(255,255,255,1)
+}
