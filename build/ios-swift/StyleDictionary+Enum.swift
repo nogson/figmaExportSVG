ff0faf5fcbb4bc10d28a4565b898b94087788862
nogson <nogson@users.noreply.github.com>
2024-07-11 08:30:20 +0000

    feat: update variables

---
+++ build/ios-swift/StyleDictionary+Enum.swift
@@ -0,0 +1,14 @@
+
+//
+// StyleDictionary+Enum.swift
+//
+
+// Do not edit directly, this file was auto-generated.
+
+
+import UIKit
+
+public enum StyleDictionaryEnum {
+    public static let colorColorDark = rgba(0,0,0,1)
+    public static let colorColorLight = rgba(255,255,255,1)
+}
